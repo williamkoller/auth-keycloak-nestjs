c1dc812f0dac627337c221085d4a26d456fd3460
william koller <wkoller25@gmail.com>
2021-11-15 00:18:50 -0300

    fix: remove project kollerlabs041

--- kollerlabs041/src/modules/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './controllers/auth.controller';
-import { AuthService } from './service/auth.service';
-
-@Module({
-  providers: [AuthService],
-  controllers: [AuthController],
-})
-export class AuthModule {}
